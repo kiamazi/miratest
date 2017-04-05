05d6e221acc36f293c68bdcc7321061338f6926a
kiavash <kiavash@gmail.com>
2017-04-06 03:50:37 +0430

    fix content and statics

---
+++ static/theme/behroozam/js/run.js
@@ -0,0 +1,39 @@
+/*!
+ * Sallar.me
+ * Published under MIT License.
+ * Designed and Developed by Sallar Kaboli (sallar.kaboli@gmail.com)
+ */
+
+// Helpers
+// ------------------------------------------------------
+function hasClass(ele,cls) {
+    return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
+
+function addClass(ele,cls) {
+    if (!hasClass(ele,cls)) {
+        ele.className += " "+cls;
+    }
+}
+
+function removeClass(ele,cls) {
+    if (hasClass(ele,cls)) {
+        var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+        ele.className=ele.className.replace(reg,' ');
+    }
+}
+
+var pres = document.querySelectorAll('pre')
+  , len  = pres.length;
+
+// Pretty Print
+// ------------------------------------------------------
+for (var i = 0; i < len; i++) {
+    if (!hasClass(pres[i], 'raw')) {
+        addClass(pres[i], 'prettyprint');
+        addClass(pres[i], 'linenums');
+    }
+}
+
+prettyPrint();
+
